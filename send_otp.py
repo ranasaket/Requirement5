562d99291f3f0e09cb4a524a61497957e7213ee6
ranasaket19110 <ranasaket19110@gmail.com>
2023-01-09 22:52:45 +0530

    new

---
+++ send_otp.py
@@ -0,0 +1,37 @@
+import boto3
+import random
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    
+    sms_client= boto3.client("sns")
+    paswd=random.randint(100000,999999)
+    print('otps: ',paswd)
+    number=str(event['queryStringParameters']['mobile'])
+    print("Mobile number is :",number)
+    message= f"Your OTPs for mobile number verification is: {paswd}"
+    try:
+        ress= sms_client.publish(
+        PhoneNumber=number,
+        Message=message,
+
+        #Subject="KeepSL_OTP_Demo",
+        #MessageAttributes={
+        #        'AWS.SNS.SMS.SMSType': {
+        #        'DataType': 'String',
+        #       'StringValue': 'Transactional'
+        #    }}
+            )
+    except ClientError as er:
+        print(er.response['Error']['Message'])
+        raise er
+    else:
+      print("Text Message sent! Message ID:",ress['MessageId']),
+    
+    
+    res="OTP sent to the Customer was: "+str(paswd)
+
+
+    return res
+    
+    
